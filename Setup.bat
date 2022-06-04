520706d86e01e9b1f5a842e69d3855b64013e7dd
SuperCraftAlex <63254202+SuperCraftAlex@users.noreply.github.com>
2022-06-04 20:45:04 +0200

    init

---
+++ Setup.bat
@@ -0,0 +1,73 @@
+@echo off
+title VirusConsole setup
+echo Ready to setup VirusConsole.
+pause
+
+echo Enabling files...
+
+rename Console.bat.dis Console.bat
+rename emoji.bat.dis emoji.bat
+rename Restarter.bat.dis Restarter.bat
+
+echo Create desktop shortcut
+:createshort
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\Console.lnk" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%cd%\Console.bat" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
+
+echo Creating config...
+:createconfig
+echo #Main Color >> Console.cfg
+echo 0A >> Console.cfg
+echo #Color Input Dialoge >> Console.cfg
+echo 92 >> Console.cfg
+echo #Color Command Output >> Console.cfg
+echo 94 >> Console.cfg
+echo #Color Input Text >> Console.cfg
+echo 91 >> Console.cfg
+echo #Console.bat PATH >> Console.cfg
+echo %cd%\Console.bat >> Console.cfg
+echo #Library PATH >> Console.cfg
+echo %cd%\Librarys >> Console.cfg
+echo #Tools PATH >> Console.cfg
+echo %cd%\Tools >> Console.cfg
+echo #Restarter PATH >> Console.cfg
+echo %cd%\Restarter.bat >> Console.cfg
+echo #Emoji PATH >> Console.cfg
+echo %cd%\emoji.bat >> Console.cfg
+echo #Code Folder PATH >> Console.cfg
+echo %cd% >> Console.cfg
+
+echo Installing tools...
+:installtools
+curl -o temp.zip https://supercraftalex.000webhostapp.com/Tools.zip
+
+echo unzipping...
+Call :UnZipFile "%cd%\Tools" "%cd%\temp.zip"
+
+del /f temp.zip
+
+echo Installation finished
+pause
+exit /b
+
+
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
