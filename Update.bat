ba6b8a3a660e784beb84c5c8b6a7a0374dbfe377
SuperCraftAlex <63254202+SuperCraftAlex@users.noreply.github.com>
2022-06-04 21:01:53 +0200

    Create Update.bat

---
+++ Update.bat
@@ -0,0 +1,44 @@
+@echo off
+title Updater
+
+echo Ready to update
+pause
+curl -o Console.bat https://raw.githubusercontent.com/SuperCraftAlex/AdvancedWindowsConsole/main/Console.bat.dis
+
+echo Update finished. If something doesnt work then rerun this script and press [2].
+echo Press [1] to continue.
+
+SET /P M=
+IF %M%==1 goto end
+IF %M%==2 goto reconfig
+
+:end
+exit
+
+:reconfig
+del /f Console.cfg
+
+echo #Main Color >> Console.cfg
+echo 0A >> Console.cfg
+echo #Color Input Dialoge >> Console.cfg
+echo 92 >> Console.cfg
+echo #Color Command Output >> Console.cfg
+echo 94 >> Console.cfg
+echo #Color Input Text >> Console.cfg
+echo 91 >> Console.cfg
+echo #Console.bat PATH >> Console.cfg
+echo %cd%\Console.bat >> Console.cfg
+echo #Library PATH >> Console.cfg
+echo %cd%\Librarys >> Console.cfg
+echo #Tools PATH >> Console.cfg
+echo %cd%\Tools >> Console.cfg
+echo #Restarter PATH >> Console.cfg
+echo %cd%\Restarter.bat >> Console.cfg
+echo #Emoji PATH >> Console.cfg
+echo %cd%\emoji.bat >> Console.cfg
+echo #Code Folder PATH >> Console.cfg
+echo %cd% >> Console.cfg
+
+echo Everything should work now! If something is still broken, please read the update log in the github repository!
+pause
+exit
